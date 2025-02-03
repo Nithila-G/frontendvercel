76d55d4ec17b84edfe161fe6ca26f4298126af2e
Gowdhami21MID0161 <gowdhami.2021@vitstudent.ac.in>
2025-02-03 21:15:31 +0530

    Update findforms.jsx

--- src/pages/home/components/findforms.jsx
+++ src/pages/home/components/findforms.jsx
@@ -4,11 +4,10 @@ import { ToastContainer, toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom'; // Import useNavigate
 import 'react-toastify/dist/ReactToastify.css';
 
-// Import images from the assets folder
-import CleaningImage from '/src/assets/Cleaning.png';
-import CookingImage from '/src/assets/Cooking.png';
-import BabySittingImage from '/src/assets/Baby-Sitting.png';
-import ElderCareImage from '/src/assets/Elder Care.png';
+import CleaningImage from '../../../assets/Cleaning.png';
+import CookingImage from '../../../assets/Cooking.png';
+import BabySittingImage from '../../../assets/Baby-Sitting.png';
+import ElderCareImage from '../../../assets/Elder Care.png';
 
 const MultiStepFormComponent = () => {
   const [formData, setFormData] = useState({
