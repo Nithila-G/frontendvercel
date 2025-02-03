6e75636c658cc2566de998b0e366a53847dce896
GowdhamiJ <jgowdhami10@gmail.com>
2025-02-03 20:05:08 +0530

    Resolved merge conflicts

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,6 +1,5 @@
 import menu from '../assets/menu.png';
 import back from '../assets/dropdown_icon.png';
-<<<<<<< HEAD
 import banner from '../assets/homeimg.jpg';
 import shine from '../assets/star.png';
 import cleaning from '../assets/Cleaning.png';
@@ -12,17 +11,3 @@ export const assets = {
   shine,
   cleaning,
 };
-=======
-import banner from '../assets/homeimg.jpg'
-import shine from '../assets/star.png'
-import cleaning from "../assets/Cleaning.png"
-
-export const assets = {
-    menu,
-    back,
-    banner,
-    shine,
-    cleaning,
-    
-}
->>>>>>> 9b1e728d22300411eef54ee530de7be9a93df5b8
