98e1e20cf11ef20e70237ff1ce1a04b5e6d99848
GowdhamiJ <jgowdhami10@gmail.com>
2025-02-03 20:02:05 +0530

    C.png

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,14 +1,13 @@
 import menu from '../assets/menu.png';
 import back from '../assets/dropdown_icon.png';
-import banner from '../assets/homeimg.jpg'
-import shine from '../assets/star.png'
-import cleaning from "../assets/cleaning.png"
+import banner from '../assets/homeimg.jpg';
+import shine from '../assets/star.png';
+import cleaning from '../assets/Cleaning.png';
 
 export const assets = {
-    menu,
-    back,
-    banner,
-    shine,
-    cleaning,
-    
-}
+  menu,
+  back,
+  banner,
+  shine,
+  cleaning,
+};
