54b036e1fcf5c219fdf0ffc972b1f945fa46c087
Gowdhami21MID0161 <gowdhami.2021@vitstudent.ac.in>
2025-02-03 21:09:08 +0530

    Update assets.js

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -2,7 +2,7 @@ import menu from './menu.png';
 import back from './dropdown_icon.png';
 import banner from './homeimg.jpg';
 import shine from './star.png';
-import cleaning from '../Cleaning.png';
+import cleaning from './Cleaning.png';
 
 export const assets = {
   menu,
