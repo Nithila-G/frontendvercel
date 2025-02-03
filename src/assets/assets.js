441cf62521f2f1cd29b898a35a2de8d1a28f3871
GowdhamiJ <jgowdhami10@gmail.com>
2025-02-03 21:40:24 +0530

    f

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -2,12 +2,10 @@ import menu from '../assets/menu.png';
 import back from '../assets/dropdown_icon.png';
 import banner from '../assets/homeimg.jpg';
 import shine from '../assets/star.png';
-//import cleaning from '../assets/cleaning.png';
 
 export const assets = {
   menu,
   back,
   banner,
   shine,
-  //cleaning,
 };
